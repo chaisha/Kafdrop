1ec3721f9b91a2d8b82e9290ca85daefc09e958a
Chaitra Shanthappa <chaitra.shanthappa@paf.com>
2018-09-14 17:11:05 +0300

    integrate schema registry to parse avro messages

--- src/main/java/com/homeadvisor/kafdrop/service/MessageInspector.java
+++ src/main/java/com/homeadvisor/kafdrop/service/MessageInspector.java
@@ -22,6 +22,9 @@ import com.homeadvisor.kafdrop.model.MessageVO;
 import com.homeadvisor.kafdrop.model.TopicPartitionVO;
 import com.homeadvisor.kafdrop.model.TopicVO;
 import com.homeadvisor.kafdrop.util.BrokerChannel;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import kafka.api.FetchRequest;
 import kafka.api.FetchRequestBuilder;
 import kafka.javaapi.FetchResponse;
@@ -29,16 +32,26 @@ import kafka.javaapi.consumer.SimpleConsumer;
 import kafka.javaapi.message.ByteBufferMessageSet;
 import kafka.message.Message;
 import kafka.message.MessageAndOffset;
+import org.apache.commons.lang.StringUtils;
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.rest.RestService;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -50,46 +63,107 @@ public class MessageInspector
    @Autowired
    private KafkaMonitor kafkaMonitor;
 
+   @Value("${schema.registry}")
+   private String schemaRegistry;
+
+
+   private SchemaRegistryClient schemaRegistryClient;
+
+   @PostConstruct
+   public void init() {
+       List<String> urls = Arrays.stream(schemaRegistry.split(",")).map(String::trim).collect(Collectors.toList());
+       this.schemaRegistryClient = new CachedSchemaRegistryClient(urls,1000);
+   }
+
    public List<MessageVO> getMessages(String topicName, int partitionId, long offset, long count)
    {
       final TopicVO topic = kafkaMonitor.getTopic(topicName).orElseThrow(TopicNotFoundException::new);
       final TopicPartitionVO partition = topic.getPartition(partitionId).orElseThrow(PartitionNotFoundException::new);
 
-      return kafkaMonitor.getBroker(partition.getLeader().getId())
-         .map(broker -> {
-            SimpleConsumer consumer = new SimpleConsumer(broker.getHost(), broker.getPort(), 10000, 100000, "");
+       List<MessageVO> messages = new ArrayList<>();
+       try {
+           if (!schemaRegistryClient.getAllSubjects().contains(getSchemaName(topicName))) {
+               LOG.info("Not an AVRO message, no magic byte found. display the message as it is");
+                kafkaMonitor.getBroker(partition.getLeader().getId())
+                       .map(broker -> {
+                           /** non avro messages*/
 
-            final FetchRequestBuilder fetchRequestBuilder = new FetchRequestBuilder()
-               .clientId("KafDrop")
-               .maxWait(5000) // todo: make configurable
-               .minBytes(1);
+                           long currentOffset1 = offset;
+                           SimpleConsumer simpleConsumer = new SimpleConsumer(broker.getHost(), broker.getPort(), 10000, 100000, "");
 
-            List<MessageVO> messages = new ArrayList<>();
-            long currentOffset = offset;
-            while (messages.size() < count)
-            {
-               final FetchRequest fetchRequest =
-                  fetchRequestBuilder
-                     .addFetch(topicName, partitionId, currentOffset, 1024 * 1024)
-                     .build();
+                           final FetchRequestBuilder fetchRequestBuilder = new FetchRequestBuilder()
+                                   .clientId("KafDrop")
+                                   .maxWait(5000) // todo: make configurable
+                                   .minBytes(1);
 
-               FetchResponse fetchResponse = consumer.fetch(fetchRequest);
+                           long currentOffset = offset;
+                           while (messages.size() < count) {
+                               final FetchRequest fetchRequest =
+                                       fetchRequestBuilder
+                                               .addFetch(topicName, partitionId, currentOffset, 1024 * 1024)
+                                               .build();
 
-               final ByteBufferMessageSet messageSet = fetchResponse.messageSet(topicName, partitionId);
-               if (messageSet.validBytes() <= 0) break;
+                               FetchResponse fetchResponse = simpleConsumer.fetch(fetchRequest);
 
+                               final ByteBufferMessageSet messageSet = fetchResponse.messageSet(topicName, partitionId);
+                               if (messageSet.validBytes() <= 0) break;
 
-               int oldSize = messages.size();
-               StreamSupport.stream(messageSet.spliterator(), false)
-                  .limit(count - messages.size())
-                  .map(MessageAndOffset::message)
-                  .map(this::createMessage)
-                  .forEach(messages::add);
-               currentOffset += messages.size() - oldSize;
-            }
-            return messages;
-         })
-         .orElseGet(Collections::emptyList);
+                               int oldSize = messages.size();
+                               StreamSupport.stream(messageSet.spliterator(), false)
+                                       .limit(count - messages.size())
+                                       .map(MessageAndOffset::message)
+                                       .map(this::createMessage)
+                                       .forEach(messages::add);
+                               currentOffset += messages.size() - oldSize;
+                           }
+                           return messages;
+                       })
+                       .orElseGet(Collections::emptyList);
+
+           } else {
+                LOG.info("Avro schema found");
+                kafkaMonitor.getBroker(partition.getLeader().getId())
+                       .map(broker -> {
+                           Properties consumerProps = new Properties();
+                           String brokerAddress = broker.getHost() + ":" + broker.getPort();
+                           consumerProps.put("bootstrap.servers", brokerAddress);
+                           consumerProps.put("schema.registry.url", schemaRegistry);
+                           consumerProps.put("group.id", "KafDrop");
+                           consumerProps.put("key.deserializer",
+                                   "org.apache.kafka.common.serialization.StringDeserializer");
+                           consumerProps.put("value.deserializer",
+                                   "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+                           consumerProps.put("auto.offset.reset","earliest");
+                           KafkaConsumer<String, GenericRecord> consumer = new KafkaConsumer<>(consumerProps);
+                           TopicPartition partition0 = new TopicPartition(topicName, partitionId);
+
+                           consumer.assign(Arrays.asList(partition0));
+                           consumer.poll(1); // a quick poll is needed before we seek tothe position requested
+                           consumer.seek(partition0, offset);
+
+                           long currentOffset1 = offset;
+                               while (messages.size() < count) {
+                                   ConsumerRecords<String, GenericRecord> records = consumer.poll(5000); // todo: make timeout configurable
+                                   for (ConsumerRecord<String, GenericRecord> record : records) {
+                                       MessageVO mess = new MessageVO();
+                                       mess.setMessage(record.value().toString());
+                                       mess.setKey(record.key());
+                                       mess.setChecksum(0);
+                                       mess.setCompressionCodec("none");
+                                       messages.add(mess);
+                                       if (messages.size() == count)
+                                           break;
+                                   }
+                               }
+                           return messages;
+                       })
+                       .orElseGet(Collections::emptyList);
+             }
+           }
+           catch(Exception e) {
+                LOG.error(e.getMessage());
+           }
+       return messages;
    }
 
    private MessageVO createMessage(Message message)
@@ -123,6 +197,12 @@ public class MessageInspector
          return "<unsupported encoding>";
       }
    }
+
+    public String getSchemaName(String topicName) {
+
+        return String.format("%s-value", topicName);
+
+    }
 
    private byte[] readBytes(ByteBuffer buffer)
    {
